b83ff27751b3449f07e58870abb75bd46e0147f3
emikes97 <mikesemmanouil@outlook.com.gr>
2025-08-12 12:31:15 +0300

    Initialized the portfolio project

---
+++ E-Shop/core/src/test/java/commerse/eshop/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package commerse.eshop.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
