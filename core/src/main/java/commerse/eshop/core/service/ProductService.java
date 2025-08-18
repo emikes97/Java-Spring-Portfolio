2975084e92ffbfcb6dd381aa01598967fecf095c
emikes97 <mikesemmanouil@outlook.com.gr>
2025-08-18 21:34:11 +0300

    Saving progress

--- core/src/main/java/commerse/eshop/core/service/ProductService.java
+++ core/src/main/java/commerse/eshop/core/service/ProductService.java
@@ -1,6 +1,23 @@
 package commerse.eshop.core.service;
 
+import commerse.eshop.core.web.dto.requests.Products.DTOAddProduct;
+import commerse.eshop.core.web.dto.response.Product.DTOProductResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
 public interface ProductService {
 
+    // == Add New Product
+    DTOProductResponse addProduct(DTOAddProduct dto);
 
+    // == Remove Product
+    void removeProduct(long id);
+
+    // == Fetch Product
+    Optional<DTOProductResponse> getProduct(long id);
+
+    // == Fetch all products of a category
+    Page<DTOProductResponse> getAllProducts(String category, long categoryId, Pageable pageable);
 }
---
+++ core/src/main/java/commerse/eshop/core/web/dto/requests/Products/DTOAddProduct.java
@@ -0,0 +1,14 @@
+package commerse.eshop.core.web.dto.requests.Products;
+
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+public record DTOAddProduct(@NotBlank @Size(max = 200) String productName,
+                            @NotBlank @Size(max = 255) String productDescription,
+                            @NotNull Map<String, Object> productDetails,
+                            @NotNull @Min(0) int productAvailableStock,
+                            @NotNull @DecimalMin("0.001") BigDecimal productPrice,
+                            boolean isActive) {
+}
