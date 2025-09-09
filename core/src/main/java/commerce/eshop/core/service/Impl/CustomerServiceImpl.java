72e1114c52e264e8dd74cf83d3df4c3a872731e8
emikes97 <mikesemmanouil@outlook.com.gr>
2025-09-09 13:59:54 +0300

    1

--- core/src/main/java/commerce/eshop/core/service/Impl/CustomerServiceImpl.java
+++ core/src/main/java/commerce/eshop/core/service/Impl/CustomerServiceImpl.java
@@ -25,8 +25,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @Service
