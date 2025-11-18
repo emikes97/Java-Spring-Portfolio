14c20bddc955c24d60ba3826a5826c7b60e2075c
emikes97 <mikesemmanouil@outlook.com.gr>
2025-11-18 12:10:30 +0200

    Test units started for address service

---
+++ core/src/test/java/commerse/eshop/core/application/customerTest/addons/address/writer/AddressWriterTest.java
@@ -0,0 +1,23 @@
+package commerse.eshop.core.application.customerTest.addons.address.writer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AddressWriterTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void clearDefault() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
