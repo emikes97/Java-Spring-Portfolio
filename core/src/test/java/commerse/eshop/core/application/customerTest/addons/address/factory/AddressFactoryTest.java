e86f6a695e14545d214a5ff4e544b43db8bd39ac
emikes97 <mikesemmanouil@outlook.com.gr>
2025-11-22 20:52:18 +0200

    Saving progress, all cases for customer address actions have been tested.

---
+++ core/src/test/java/commerse/eshop/core/application/customerTest/addons/address/factory/AddressFactoryTest.java
@@ -0,0 +1,51 @@
+package commerse.eshop.core.application.customerTest.addons.address.factory;
+
+import commerce.eshop.core.application.customer.addons.address.factory.AddressFactory;
+import commerce.eshop.core.model.entity.Customer;
+import commerce.eshop.core.model.entity.CustomerAddress;
+import commerce.eshop.core.web.dto.requests.CustomerAddr.DTOAddCustomerAddress;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Currency;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AddressFactoryTest {
+
+    private AddressFactory addressFactory;
+
+    @BeforeEach
+    void setUp() {
+        addressFactory = new AddressFactory();
+    }
+
+    @Test
+    void handle() {
+        Customer customer = mock(Customer.class);
+        UUID customerId = UUID.randomUUID();
+
+        when(customer.getCustomerId()).thenReturn(customerId);
+
+        DTOAddCustomerAddress dto = new DTOAddCustomerAddress(
+                "Greece",
+                "Acharnon 55",
+                "Athens",
+                "10433",
+                true
+        );
+
+        CustomerAddress address = addressFactory.handle(dto, customer);
+
+        assertNotNull(address);
+        assertSame(customer, address.getCustomer());
+        assertEquals("Greece", address.getCountry());
+        assertEquals("Acharnon 55", address.getStreet());
+        assertEquals("Athens", address.getCity());
+        assertEquals("10433", address.getPostalCode());
+        assertTrue(address.isDefault());
+    }
+}
